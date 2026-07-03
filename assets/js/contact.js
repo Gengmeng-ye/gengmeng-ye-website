4737a6ccb1785b97f40d22d26fea780c0fa94845
Prince Otchere <me@prince-us.com>
2026-07-03 17:58:44 -0400

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,28 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_b6swgum', 'template_pj1f9pc', '#contact-form', '4cHcNxeNSNFlOaDzs')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
